f26468153a165bf5d38e885b52ee3f07228b3cbf
jerome <ambroise.jerome.formation@gmail.com>
2019-05-13 11:15:41 +0200

    Premier fichier PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ma page</h1>
+    <?php
+
+        echo 'Bonjour';
+
+    ?>
+
+</body>
+</html>
