47f5c7d62285f89eb4b5ffc88216895be5cd907a
jerome <ambroise.jerome.formation@gmail.com>
2019-05-14 16:43:02 +0200

    Creation form HTML creation produit

---
+++ public/product-new.php
@@ -0,0 +1,39 @@
+<?php require 'inc/header.php' ; ?>
+
+<main class="container">
+
+    <h1>Ajout d'un produit</h1>
+
+    <form>
+        <div class="form-group">
+            <label for="name">Nom</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="price">Prix</label>
+            <input type="number" min="0" step="0.01" class="form-control" id="price" name="price">
+        </div>
+        <div class="custom-control custom-switch">
+            <input type="checkbox" class="custom-control-input" id="ispublished" name="ispublished">
+            <label class="custom-control-label" for="ispublished">Le produit doit-il être publié ?</label>
+        </div>
+        <div class="form-group">
+            <label for="createdat">Date de création</label>
+            <input type="date" class="form-control" id="createdat" name="createdat">
+        </div>
+        <div class="form-group">
+            <label for="nbviews">Nombre de vues</label>
+            <input type="number" min="0" class="form-control" id="nbviews" name="nbviews">
+        </div>
+
+        <input type="submit" value="Créer le produit" class="btn btn-outline-success">
+
+    </form>
+
+</main>
+
+<?php require 'inc/footer.php' ; ?>
--- public/tableau.php
+++ public/tableau.php
@@ -1,41 +1,82 @@
-<?php require 'inc/header.php' ?>
+<?php require 'inc/header.php' ;
+
+    $products = [
+        [
+            "name" => "Hamac",
+            "description" => "Pour se reposer après 5 jours de PHP",
+            "image_name" => "hamac.jpg"
+        ],[
+            "name" => "Parasol",
+            "description" => "Pour faire de l'ombre au Hamac",
+            "image_name" => "parasol.jpg"
+        ]
+    ];
+
+    $ballon = [
+        "name" => "Ballon",
+        "description" => "Pour jouer",
+        "image_name" => "ballon.jpg"
+    ];
+    $products[] = $ballon;
+
+    // On souhaite récupérer le Hamac
+    //var_dump($products[0]);
+    // On souhaite récupérer le nom du Hamac
+    //var_dump($products[0]['name']);
+
+    $kiwi = "kiwi";
+
+    $fruits = ["pomme", $kiwi];
+    // Syntaxe équivalente
+    $fruits = array("pomme", $kiwi);
+    $fruits[] = "pamplemousse";
+
+    var_dump($fruits[2]);
+    // La fonction "sizeof" permet de récupérer la taille d'un tableau
+    $length = sizeof($fruits);
+    // On peut utiliser une boucle "for" pour parcourir un tableau
+    for($i=0;$i<$length;$i++) {
+        // On récupère l'élément courant avec $fruits[$i]
+        var_dump($fruits[$i]);
+    }
+
+    // Tri du tableau
+    var_dump($fruits);
+    sort($fruits);
+    var_dump($fruits);
+
+    // Transformer un tableau en chaîne de caractères
+    $strFruits = implode("</li><li>", $fruits);
+    // kiwi</li><li>pamplemousse</li><li>pomme
+
+    echo "<ul><li>" . $strFruits . "</li></ul>";
+
+    $tabLegumes = ['aubergine', 'carottes', 'choux fleur'];
+    $strLegumes = implode("---",$tabLegumes);
+
+    echo $strLegumes; // aubergine---carottes---choux fleur
+
+?>
 
 <main class="container">
-    <h1>Les tableaux</h1>
+    <h1 class="mt-2">Présentation des produits</h1>
     <section class="row">
-    <?php
-        $products = [
-            [
-                "name" => "Hamac",
-                "description" => "Pour se reposer après 5 jours de PHP",
-                "image_name" => "hamac.jpg"
-            ],[
-                "name" => "Parasol",
-                "description" => "Pour faire de l'ombre au Hamac",
-                "image_name" => "parasol.jpg"
-            ],
-            [
-                "name" => "Ballon",
-                "description" => "Pour jouer",
-                "image_name" => "ballon.jpg"
-            ]
-        ];
-        // On souhaite récupérer le Hamac
-        //var_dump($products[0]);
-        // On souhaite récupérer le nom du Hamac
-        //var_dump($products[0]['name']);
-
-        foreach ($products as $product) :
-    ?>
-        <div class="card col-4">
-            <img src="img/uploads/<?= $product['image_name']; ?>" class="card-img-top" alt="Image de <?= $product['name']; ?>">
-            <div class="card-body">
-                <h5 class="card-title"><?= $product['name']; ?></h5>
-                <p class="card-text"><?= $product['description']; ?></p>
-                <a href="#" class="card-link btn btn-outline-info">Détail</a>
+        <?php foreach ($products as $product) : ?>
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/uploads/<?= $product['image_name']; ?>" class="card-img-top" alt="Image de <?= $product['name']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $product['name']; ?></h5>
+                        <p class="card-text"><?= $product['description']; ?></p>
+                        <a href="#" class="card-link btn btn-outline-info">Détail</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
     </section>
 </main>
+
+
+<form action="tableau.php" method=""></form>
+
 <?php require 'inc/footer.php'; ?>
