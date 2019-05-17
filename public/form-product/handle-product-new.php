26200a59099594ecd7d1fde0565b5b982976857f
jerome <ambroise.jerome.formation@gmail.com>
2019-05-17 17:02:30 +0200

    Affichage des messages d'erreurs du formulaire

--- public/form-product/handle-product-new.php
+++ public/form-product/handle-product-new.php
@@ -6,29 +6,12 @@ require 'form-functions.php';
 // $_SERVER contient un tas de variables (navigateurs/serveurs)
 // La clef "REQUEST_METHOD" contient la méthode HTTP utilisée
 if($_SERVER['REQUEST_METHOD'] === "POST") {
-
-    /* Test du nom */
     $errorMessageName = checkPostText('name', 255);
-    var_dump($errorMessageName);
-    /* Test de description */
     $errorMessageDesc = checkPostText('description', 65535);
-    var_dump($errorMessageDesc);
-    /* Test du prix */
     $errorMessagePrice = checkPostNumber('price', 0, 9999999.99);
-    var_dump($errorMessagePrice);
-
-    /* Test checkbox etat publication */
     sanitizeRadio('ispublished');
-
-    /* Test de la date de création */
     $errorMessageCreatedAt = checkPostDate("createdat");
-    var_dump($errorMessageCreatedAt);
-
-    /* Test du nombre de vues */
     $errorMessageNbViews = checkPostNumber('nbviews', 0, 19E19, false);
-    var_dump($errorMessageNbViews);
-
-    var_dump($_POST);
 }
 
 
--- public/form-product/product-new.php
+++ public/form-product/product-new.php
@@ -3,6 +3,7 @@
 require '../inc/header.php';
 // Inclusion du fichier PHP de traitement du formulaire
 require 'handle-product-new.php';
+
 ?>
 
 <main class="container">
@@ -10,29 +11,47 @@ require 'handle-product-new.php';
     <h1>Ajout d'un produit</h1>
 
     <form method="post">
+
+        <?php  // createInputText('name', 255, "Nom") ?>
+
         <div class="form-group">
             <label for="name">Nom</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text"
+                   class="form-control <?= (isset($errorMessageName) && !empty($errorMessageName)) ? 'is-invalid' : '' ?>"
+                   id="name" name="name" value="<?= $_POST['name']  ?? '' ?>">
+            <div class="invalid-feedback"><?= $errorMessageName ?? "" ?></div>
         </div>
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+            <textarea class="form-control <?= (isset($errorMessageDesc) && !empty($errorMessageDesc)) ? 'is-invalid' : '' ?>"
+                      id="description" rows="3" name="description"><?= $_POST['description'] ?? '' ?></textarea>
+            <div class="invalid-feedback"><?= $errorMessageDesc ?? "" ?></div>
         </div>
         <div class="form-group">
             <label for="price">Prix</label>
-            <input type="number" min="0" step="0.01" class="form-control" id="price" name="price">
+            <input type="number" min="0" step="0.01"
+                   class="form-control <?= (isset($errorMessagePrice) && !empty($errorMessagePrice)) ? 'is-invalid' : '' ?>"
+                   id="price" name="price" value="<?= $_POST['price'] ?? '' ?>">
+            <div class="invalid-feedback"><?= $errorMessagePrice ?? "" ?></div>
         </div>
         <div class="custom-control custom-switch">
-            <input type="checkbox" class="custom-control-input" id="ispublished" name="ispublished">
+            <input type="checkbox"
+                   class="custom-control-input" id="ispublished" name="ispublished">
             <label class="custom-control-label" for="ispublished">Le produit doit-il être publié ?</label>
         </div>
         <div class="form-group">
             <label for="createdat">Date de création</label>
-            <input type="date" class="form-control" id="createdat" name="createdat">
+            <input type="date"
+                   class="form-control <?= (isset($errorMessageCreatedAt) && !empty($errorMessageCreatedAt)) ? 'is-invalid' : '' ?>"
+                   id="createdat" name="createdat" <?= $_POST['createdat'] ?? '' ?>>
+            <div class="invalid-feedback"><?= $errorMessageCreatedAt ?? "" ?></div>
         </div>
         <div class="form-group">
             <label for="nbviews">Nombre de vues</label>
-            <input type="number" min="0" class="form-control" id="nbviews" name="nbviews">
+            <input type="number" min="0"
+                   class="form-control <?= (isset($errorMessageNbViews) && !empty($errorMessageNbViews)) ? 'is-invalid' : '' ?>"
+                   id="nbviews" name="nbviews" <?= $_POST['nbviews'] ?? '' ?>>
+            <div class="invalid-feedback"><?= $errorMessageNbViews ?? "" ?></div>
         </div>
 
         <input type="submit" value="Créer le produit" class="btn btn-outline-success">
